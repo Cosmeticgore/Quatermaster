42b8d76fb8a183bb1be7e78db66088f812034c2d
Soger1 <C20322553@mytudublin.ie>
2025-03-08 20:53:16 +0000

    added list of players

--- FYP_Prototype/app/src/main/java/com/example/fyp_prototype/MainActivity.kt
+++ FYP_Prototype/app/src/main/java/com/example/fyp_prototype/MainActivity.kt
@@ -14,12 +14,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -34,11 +42,16 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.google.firebase.functions.FirebaseFunctions
@@ -57,16 +70,19 @@ import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+
 
 class MainActivity : ComponentActivity() {
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
     private val database = Firebase.database
     private val databaseRef = database.getReference("sessions")
     private val scope = CoroutineScope(Dispatchers.Main + Job())
-    private val userMarkers = mutableMapOf<String,Marker>()
+    private val userMarkers = mutableMapOf<String, Marker>()
     private lateinit var userdata: AppData
     private val functions: FirebaseFunctions = Firebase.functions
-
 
 
     @SuppressLint("NewApi")
@@ -86,10 +102,10 @@ class MainActivity : ComponentActivity() {
 
         val User = user(
             userId = User_ID.toString(),
-            location = user_loc(0.0,0.0),
+            location = user_loc(0.0, 0.0),
             role = role.toString()
         )
-        Intent(applicationContext, LocationService::class.java).apply{
+        Intent(applicationContext, LocationService::class.java).apply {
             action = LocationService.ACTION_START
             startService(this)
         }
@@ -102,18 +118,19 @@ class MainActivity : ComponentActivity() {
             // Get new FCM registration token
             val token = task.result
             Log.d("FCM TOKEN", token)
-            databaseRef.child(Session_ID.toString()).child("users").child(User_ID.toString()).child("not_token").setValue(token.toString()).addOnSuccessListener{
-                Log.d("FCM TOKEN", "Token Updated")
-            }.addOnFailureListener{
-                Log.d("FCM TOKEN", "Failed to upload token")
-            }
+            databaseRef.child(Session_ID.toString()).child("users").child(User_ID.toString())
+                .child("not_token").setValue(token.toString()).addOnSuccessListener {
+                    Log.d("FCM TOKEN", "Token Updated")
+                }.addOnFailureListener {
+                    Log.d("FCM TOKEN", "Failed to upload token")
+                }
 
         })
         changeteam(userdata, this)
 
         //UI
         setContent {
-            HomeScreen(User, Session_ID.toString())
+            App(User, Session_ID.toString())
         }
     }
 
@@ -133,8 +150,13 @@ class MainActivity : ComponentActivity() {
 
         for (permission in permissions) { // check to see if they are granted
             if (ContextCompat.checkSelfPermission(this, permission)
-                != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, permissions, REQUEST_PERMISSIONS_REQUEST_CODE)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    permissions,
+                    REQUEST_PERMISSIONS_REQUEST_CODE
+                )
                 break
             }
         }
@@ -149,7 +171,7 @@ class MainActivity : ComponentActivity() {
                         try {
                             val userData = userSnapshot.getValue(user::class.java)
                             if (userData != null) {
-                                if(userData.userId != user.userId && userData.team == user.team || user.role == "Admin"){ //only show location if you are an admin or they are on your team
+                                if (userData.userId != user.userId && userData.team == user.team || user.role == "Admin") { //only show location if you are an admin or they are on your team
                                     updatemarker(mapView, userData)
                                 }
 
@@ -161,6 +183,7 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 }
+
                 override fun onCancelled(error: DatabaseError) {
                     Log.e("FirebaseError", "Database query failed: ${error.message}")
                 }
@@ -228,7 +251,7 @@ class MainActivity : ComponentActivity() {
 
 
     @Composable
-    fun OsmdroidMapView(User: user, S_ID : String) { // view that displays map
+    fun OsmdroidMapView(User: user, S_ID: String) { // view that displays map
         val context = LocalContext.current
         val locationProvider = GpsMyLocationProvider(context)
         val mapView = remember { MapView(context) }
@@ -241,11 +264,15 @@ class MainActivity : ComponentActivity() {
 
         DisposableEffect(Unit) {
             val job = scope.launch { // co routine to have these tasks run with the rest of the app
-                while(isActive){ //gets the location off the location provider *TODO SWITCH THIS TO USING A FUSED LOCATION PROVIDER*
-                    if (locationProvider.lastKnownLocation != null){
+                while (isActive) { //gets the location off the location provider *TODO SWITCH THIS TO USING A FUSED LOCATION PROVIDER*
+                    if (locationProvider.lastKnownLocation != null) {
                         User.location.latitude = locationProvider.lastKnownLocation.latitude
                         User.location.longitude = locationProvider.lastKnownLocation.longitude
-                        updateLocations(mapView, User, S_ID) // gets the other users location from the database and updates them and marks them
+                        updateLocations(
+                            mapView,
+                            User,
+                            S_ID
+                        ) // gets the other users location from the database and updates them and marks them
                     }
                     delay(15000) // delay of 15 seconds between updates
                 }
@@ -279,41 +306,35 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun HomeScreen(User: user, Session_ID: String){
-        Box(modifier = Modifier.fillMaxSize()) { // UI
-            OsmdroidMapView(User,Session_ID)
+    private fun App(User: user, Session_ID: String) {
+        val navController = rememberNavController()
 
-            // Session ID Box in top left corner
-            Box(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .background(
-                        color = Color.White.copy(alpha = 0.8f),
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .padding(8.dp)
-                    .align(Alignment.TopStart)
-            ) {
-                Text( //displays the session id for the user to share
-                    text = "Session ID: $Session_ID",
-                    color = Color.Black,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Medium
-                )
+        NavHost(navController = navController, startDestination = "map") {
+            composable("map") {
+                MapHome(User, Session_ID, navController)
             }
+            composable("info") {
+                InfoScreen(navController, Session_ID)
+            }
+            composable("players") {
+                PlayersListScreen(navController, Session_ID)
+            }
+        }
 
-            Box(
+    }
+
+    @Composable
+    private fun MapHome(User: user, Session_ID: String, navController: NavController) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            OsmdroidMapView(User, Session_ID)
+
+            Row(
                 modifier = Modifier
                     .padding(16.dp)
                     .align(Alignment.BottomStart)
             ) {
                 Button(
                     onClick = { ping(Session_ID) },
-                    modifier = Modifier
-                        .background(
-                            color = Color.White.copy(alpha = 0.8f),
-                            shape = RoundedCornerShape(8.dp)
-                        ),
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color.White.copy(alpha = 0.8f)
                     )
@@ -326,9 +347,227 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+            Button(
+                onClick = { navController.navigate("info") },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White.copy(alpha = 0.8f)
+                )
+            ) {
+                Text(
+                    text = "Info",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+
+        }
+    }
+
+    @Composable
+    private fun InfoScreen(navController: NavController, Session_ID: String) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Text(
+                    text = "Session ID: $Session_ID",
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    text = "Game Brief",
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Text(
+                    text = "Lorem Ipsum",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
+            Button(
+                onClick = { navController.navigate("players") },
+                modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White.copy(alpha = 0.8f)
+                )
+            ) {
+                Text(
+                    text = "View Session Players",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+
+            Button(
+                onClick = { navController.navigateUp() },
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 32.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White.copy(alpha = 0.8f)
+                )
+            ) {
+                Text(
+                    text = "Back to Map",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun PlayersListScreen(navController: NavController, SID: String) {
+        val users = remember { mutableStateOf<List<user>>(emptyList()) }
+        val database = FirebaseDatabase.getInstance()
+        val databaseRef = database.getReference("sessions")
+
+
+        LaunchedEffect(SID) {
+            val tempusers = mutableListOf<user>()
+            databaseRef.child(SID).child("users")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) { //called when data is changed
+                    for (userSnapshot in snapshot.children) {
+                        try {
+                            val userData = userSnapshot.getValue(user::class.java)
+                            if (userData != null) {
+                                tempusers.add(
+                                    user(
+                                        userData.userId,
+                                        user_loc(
+                                            userData.location.longitude,
+                                            userData.location.latitude
+                                        ),
+                                        userData.team,
+                                        userData.role,
+                                        userData.status,
+                                        ""
+                                    )
+                                )
+                                Log.i("PlayerList", "Added User ${userData.userId}")
+
+                            } else { //error handling
+                                Log.e("FirebaseDebug", "UserData is null")
+                            }
+                        } catch (e: Exception) {
+                            Log.e("FirebaseDebug", "Error deserializing UserData", e)
+                        }
+                    }
+                    users.value = tempusers
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e("FirebaseError", "Database query failed: ${error.message}")
+                }
+
+            })}
+
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = "Session Players",
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                LazyColumn(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                ) { items(users.value) { user ->
+                    Log.i("PlayerList", "Displaying User")
+                    PlayerListItem(user)
+                    Divider()
+                }
+
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { navController.navigateUp() },
+                modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White.copy(alpha = 0.8f),
+                )
+            ) {
+                Text(
+                    text = "Back",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun PlayerListItem(user: user) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp)
+        ) {
+            Text(
+                text = "User ID: ${user.userId}",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Role: ${user.role}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+                Text(
+                    text = "Status: ${user.status}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Team: ${user.team}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
         }
     }
 }
+
 
 
 
